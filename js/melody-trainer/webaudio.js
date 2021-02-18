879444dae0b0c2a9da14e9d85411c94e534a63d8
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-02-17 21:53:41 -0500

    reconnects webAudio thru promise. probable performance increase. cadence overlap fixed

--- js/melody-trainer/webaudio.js
+++ js/melody-trainer/webaudio.js
@@ -1,7 +1,14 @@
 // WebAudio Digital Instrument for "Music Trainer"
 
 // array of instrument sources
-let instrumentBank = [pianoSource, tenorSaxSource, pipeOrganSource, frenchHornSource, violinSource, marimbaSource];
+let instrumentBank = [
+    pianoSource, 
+    tenorSaxSource, 
+    pipeOrganSource, 
+    frenchHornSource, 
+    violinSource, 
+    marimbaSource
+];
 
 function powerSwitch() {
 
@@ -58,13 +65,10 @@ function instrumentCycle() {
     function soundLoader() {
 
         for (let i = 0; i <= noteButtonArray.length - 1; i++) {
-
             // assign event listener to html element
             noteButtonArray[i].addEventListener('mousedown', playSound);
-
-            // webAudio track connection
+            // webAudio track connect to destination
             instrumentChoice[1][i].connect(audioCx.destination);
-
         }
 
     }
@@ -72,13 +76,13 @@ function instrumentCycle() {
 
 // Promise object returning on audio end
 function playNotePromise(url) {
-    return new Promise(function (resolve, reject) { // return a promise
-        var audio = new Audio(url); // create audio wo/ src
+    return new Promise( (resolve, reject) => {
+        // create obj with webAudio node
+        var audio = url;
         currentAudio = audio;
-        audio.play() // autoplay when loaded
-        audio.onerror = reject; // on error, reject
-        audio.onended = resolve; // when done, resolve
-
+        audio.play();
+        audio.onerror = reject;
+        audio.onended = resolve;
     });
 }
 
@@ -103,22 +107,21 @@ function playSound() {
     // animate sounding note
     lightUp(this.parentElement);
 
+    console.log(instrumentChoice[0][i].src);
+    console.log(instrumentChoice[0][i]);
+
     // play note and repeat note condition
     if (noteButtonArray[i].dataset.playing === 'false') {
-
-        playNotePromise(instrumentChoice[0][i].src).then(function () {
+        playNotePromise(instrumentChoice[0][i]).then(function () {
             noteButtonArray[i].dataset.playing = 'false'
         });
-
         noteButtonArray[i].dataset.playing = 'true';
     } else if (noteButtonArray[i].dataset.playing === 'true') {
         currentAudio.pause();
         currentAudio.currentTime = 0;
-
-        playNotePromise(instrumentChoice[0][i].src).then(function () {
+        playNotePromise(instrumentChoice[0][i]).then(function () {
             noteButtonArray[i].dataset.playing = 'false'
         });
-
         noteButtonArray[i].dataset.playing = 'true';
     }
 
@@ -143,7 +146,7 @@ function playSound() {
 
 }
 
-// webAudio declared function for noteSwitch
+// webAudio function for noteSwitch
 function playNote(index, noteId) {
     let item = document.getElementById(`${noteId}-wrap`);
     // console.log(item);
@@ -156,7 +159,6 @@ function playNote(index, noteId) {
     if (audioCx.state === 'suspended') {
         audioCx.resume();
     }
-
     // stop currently playing audio
     if (currentAudio === undefined || cadenceBool == true) {
         // do nothing 
@@ -164,7 +166,6 @@ function playNote(index, noteId) {
         currentAudio.pause();
         currentAudio.currentTime = 0;
     }
-    
     instrumentChoice[0][index].play();
     currentAudio = instrumentChoice[0][index];
 }
@@ -176,81 +177,62 @@ function noteSwitch(noteId) {
         case "C5":
             playNote(0, noteId);
             break;
-
         case "B4":
             playNote(1, noteId);
             break;
-
         case "Bb4":
             playNote(2, noteId);
             break;
-
         case "A4":
             playNote(3, noteId);
             break;
-
         case "Ab4":
             playNote(4, noteId);
             break;
-
         case "G4":
             playNote(5, noteId);
             break;
-
         case "Gb4":
             playNote(6, noteId);
             break;
-
         case "F4":
             playNote(7, noteId);
             break;
-
         case "E4":
             playNote(8, noteId);
             break;
-
         case "Eb4":
             playNote(9, noteId);
             break;
-
         case "D4":
             playNote(10, noteId);
             break;
-
         case "Db4":
             playNote(11, noteId);
             break;
-
         case "C4":
             playNote(12, noteId);
             break;
-
         case "B3":
-
             playNote(13, noteId);
             break;
-
         case "Bb3":
             playNote(14, noteId);
             break;
-
         case "A3":
             playNote(15, noteId);
             break;
-
         case "Ab3":
             playNote(16, noteId);
             break;
-
         case "G3":
             playNote(17, noteId);
             break;
-
         case "Gb3":
             playNote(18, noteId);
             break;
-
         default:
+            console.log('Fin.')
     }
 }
 
