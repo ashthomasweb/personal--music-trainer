68f29235a07adaa80322a9b50e93637068abbd05
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-03-09 19:24:24 -0500

    add tempoArray slider and available tempo check

--- js/tempo-trainer/tempo.js
+++ js/tempo-trainer/tempo.js
@@ -63,6 +63,57 @@ function resetTempo() {
 
 }
 
+let tempoArray = [
+  20, 30, 40, 50, 55, 60, 66, 72, 76, 80, 92, 96, 100, 105, 110, 120, 126, 132, 140, 144, 150, 160, 170, 180, 190, 200, 210, 220, 230, 240
+];
 
+function startTraining() {
+  // get tempo range tested
+  lowRange.value;
+  highRange.value;
+  let newArray = [...tempoArray];
+  console.log(newArray);
+
+  function getTempos() {
+    for ( let i = 0; i < lowRange.value - 1; i++ ) {
+      newArray.shift();
+    }
+    for ( let i = 0; i < 30 - highRange.value; i++ ) {
+      newArray.pop();
+    }
+    return newArray;
+  }
+
+  return getTempos();
+
+  // get tempo sensitivity
+
+  // pick from tempoArray
+
+  // send start message
+
+  // listen for tempo
+
+  // check tempo against question
+
+  // send messgae 
+
+}
+
+// number of chords
+var lowRange = document.getElementById("lowRange");
+var highRange = document.getElementById("highRange");
+
+var lowOutput = document.getElementById("low-num");
+
+lowRange.oninput = function () {
+  lowOutput.innerHTML = tempoArray[this.value - 1];
+}
+
+var highOutput = document.getElementById("high-num");
+
+highRange.oninput = function () {
+  highOutput.innerHTML = tempoArray[this.value - 1];
+}
 
 // END of document
