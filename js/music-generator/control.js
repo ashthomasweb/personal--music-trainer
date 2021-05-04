63b397e64a08f799df9915b9c615c366a970d7b4
AshleyThomasDevelopment <ashleythomasdevelopment@gmail.com>
2021-05-03 20:59:55 -0400

    hide nonfunctioning options for diagnostics and refactor

--- js/music-generator/control.js
+++ js/music-generator/control.js
@@ -2,54 +2,54 @@ const generateChance = (factor, addend = 0) => Math.ceil(Math.random() * factor)
 
 // onscreen assigned options
 let onScreenFirstPassOptions = {
-    keyMode: undefined,
     keyCenter: undefined,
-    numOfRepeats: undefined,
-    startingChord: undefined,
-    typeOfCadence: undefined,
-    numOfChords: undefined,
-    persistState: undefined,
-    keyModeSwitch: undefined
+    // keyMode: undefined,
+    // numOfRepeats: undefined,
+    // startingChord: undefined,
+    // typeOfCadence: undefined,
+    // numOfChords: undefined,
+    // persistState: undefined,
+    // keyModeSwitch: undefined
 }
 
 function masterControl() {
     // if onscreen assigned options are manipulated, use them
     let controlOptions = onScreenFirstPassOptions;
 
-    if (controlOptions.numOfRepeats !== undefined) {
-        numOfRepeatsConBool = true;
-        numOfRepeatsOption = controlOptions.numOfRepeats;
-    }
+    // if (controlOptions.numOfRepeats !== undefined) {
+    //     numOfRepeatsConBool = true;
+    //     numOfRepeatsOption = controlOptions.numOfRepeats;
+    // }
 
-    if (controlOptions.keyMode !== undefined) {
-        keyModeConBool = true;
-        keyModeOption = controlOptions.keyMode;
-    }
+    // if (controlOptions.keyMode !== undefined) {
+    //     keyModeConBool = true;
+    //     keyModeOption = controlOptions.keyMode;
+    // }
 
     if (controlOptions.keyCenter !== undefined) {
         keyCenterConBool = true;
         keyCenterOption = controlOptions.keyCenter;
     }
 
-    if (controlOptions.startingChord !== undefined) {
-        startingChordConBool = true;
-        startingChordOption = controlOptions.startingChord;
-    }
+    // if (controlOptions.startingChord !== undefined) {
+    //     startingChordConBool = true;
+    //     startingChordOption = controlOptions.startingChord;
+    // }
 
-    if (controlOptions.typeOfCadence !== undefined) {
-        typeOfCadenceConBool = true;
-        typeOfCadenceOption = controlOptions.typeOfCadence;
-    }
+    // if (controlOptions.typeOfCadence !== undefined) {
+    //     typeOfCadenceConBool = true;
+    //     typeOfCadenceOption = controlOptions.typeOfCadence;
+    // }
 
-    if (controlOptions.numOfChords !== undefined) {
-        numOfChordsConBool = true;
-        numOfChordsOption = controlOptions.numOfChords;
-    }
+    // if (controlOptions.numOfChords !== undefined) {
+    //     numOfChordsConBool = true;
+    //     numOfChordsOption = controlOptions.numOfChords;
+    // }
 
-    if (controlOptions.persistState !== undefined) {
-        persistStateConBool = true;
-        persistStateOption = controlOptions.persistState;
-    }
+    // if (controlOptions.persistState !== undefined) {
+    //     persistStateConBool = true;
+    //     persistStateOption = controlOptions.persistState;
+    // }
 
     // if (controlOptions.keyModeSwitch !== undefined) {
     //     keyModeSwitchConBool = true;
@@ -61,52 +61,52 @@ function masterControl() {
 
 
 
-function sectionModeShift() {
-    keyCenterPersist = !keyCenterPersist;
-    if ( keyCenterPersist === true ) {
-        document.getElementById('section-shift-persist-toggle').style.backgroundColor = 'pink';
-    } else {
-        document.getElementById('section-shift-persist-toggle').style.backgroundColor = 'white';
-    }
-}
+// function sectionModeShift() {
+//     keyCenterPersist = !keyCenterPersist;
+//     if ( keyCenterPersist === true ) {
+//         document.getElementById('section-shift-persist-toggle').style.backgroundColor = 'pink';
+//     } else {
+//         document.getElementById('section-shift-persist-toggle').style.backgroundColor = 'white';
+//     }
+// }
 
-function numChordPersist() {
-    numOfChordsPersist = !numOfChordsPersist;
-    if ( numOfChordsPersist === true ) {
-        document.getElementById('num-chord-persist-toggle').style.backgroundColor = 'pink';
-    } else {
-        document.getElementById('num-chord-persist-toggle').style.backgroundColor = 'white';
-    }
-}
+// function numChordPersist() {
+//     numOfChordsPersist = !numOfChordsPersist;
+//     if ( numOfChordsPersist === true ) {
+//         document.getElementById('num-chord-persist-toggle').style.backgroundColor = 'pink';
+//     } else {
+//         document.getElementById('num-chord-persist-toggle').style.backgroundColor = 'white';
+//     }
+// }
 
-function startChordPersist() {
-    startingChordPersist = !startingChordPersist;
-    if ( startingChordPersist === true ) {
-        document.getElementById('start-chord-persist-toggle').style.backgroundColor = 'pink';
-    } else {
-        document.getElementById('start-chord-persist-toggle').style.backgroundColor = 'white';
-    }
-}
+// function startChordPersist() {
+//     startingChordPersist = !startingChordPersist;
+//     if ( startingChordPersist === true ) {
+//         document.getElementById('start-chord-persist-toggle').style.backgroundColor = 'pink';
+//     } else {
+//         document.getElementById('start-chord-persist-toggle').style.backgroundColor = 'white';
+//     }
+// }
 
-function keyModePersist() {
-    keyModePersist = !keyModePersist;
-    if ( keyModePersist === true ) {
-        document.getElementById('key-mode-persist-toggle').style.backgroundColor = 'pink';
-    } else {
-        document.getElementById('key-mode-persist-toggle').style.backgroundColor = 'white';
-    }
-}
+// function makeKeyModePersist() {
+//     keyModePersist = !keyModePersist;
+//     if ( keyModePersist === true ) {
+//         document.getElementById('key-mode-persist-toggle').style.backgroundColor = 'pink';
+//     } else {
+//         document.getElementById('key-mode-persist-toggle').style.backgroundColor = 'white';
+//     }
+// }
 
-function cadencePersist() {
-    typeOfCadencePersist = !typeOfCadencePersist;
-    if ( typeOfCadencePersist === true ) {
-        document.getElementById('cadence-persist-toggle').style.backgroundColor = 'pink';
-    } else {
-        document.getElementById('cadence-persist-toggle').style.backgroundColor = 'white';
-    }
-}
+// function cadencePersist() {
+//     typeOfCadencePersist = !typeOfCadencePersist;
+//     if ( typeOfCadencePersist === true ) {
+//         document.getElementById('cadence-persist-toggle').style.backgroundColor = 'pink';
+//     } else {
+//         document.getElementById('cadence-persist-toggle').style.backgroundColor = 'white';
+//     }
+// }
 
-function keyCenterPersist() {
+function keyCtrPersist() {
     keyCenterPersist = !keyCenterPersist;
     if ( keyCenterPersist === true ) {
         document.getElementById('key-persist-toggle').style.backgroundColor = 'pink';
@@ -120,15 +120,15 @@ function keyCenterPersist() {
 // turn off control booleans
 function turnControlOff() {
 
-    keyModeConBool = false;
     if ( keyCenterPersist === false ) {
         keyCenterConBool = false;
     }
-    numOfRepeatsConBool = false;
-    startingChordConBool = false;
-    typeOfCadenceConBool = false;
-    numOfChordsConBool = false;
-    persistStateConBool = false;
+    // keyModeConBool = false;
+    // numOfRepeatsConBool = false;
+    // startingChordConBool = false;
+    // typeOfCadenceConBool = false;
+    // numOfChordsConBool = false;
+    // persistStateConBool = false;
     // keyModeSwitchConBool = false;
 }
 
--- js/music-generator/elements.js
+++ js/music-generator/elements.js
@@ -1,85 +1,83 @@
 // Page Elements for Music Generator
 
-// number of repeats
-var numRepeatsSlider = document.getElementById("num-repeats-slider");
-var numRepeatsSliderOutput = document.getElementById("num-repeats-output");
-var repeatsCheck = document.getElementById("numOfRepeats-check");
+// // number of repeats
+// var numRepeatsSlider = document.getElementById("num-repeats-slider");
+// var numRepeatsSliderOutput = document.getElementById("num-repeats-output");
+// var repeatsCheck = document.getElementById("numOfRepeats-check");
 
-numRepeatsSliderOutput.innerHTML = '...';
-numRepeatsSlider.oninput = function () {
-    numRepeatsSliderOutput.innerHTML = this.value;
-    onScreenFirstPassOptions.numOfRepeats = this.value;
-    repeatsCheck.checked = false;
-    numOfRepeatsRandom = repeatsCheck.checked;
-    document.getElementById('num-repeats-slider').style.opacity = 0.9;
-}
+// numRepeatsSliderOutput.innerHTML = '...';
+// numRepeatsSlider.oninput = function () {
+//     numRepeatsSliderOutput.innerHTML = this.value;
+//     onScreenFirstPassOptions.numOfRepeats = this.value;
+//     repeatsCheck.checked = false;
+//     numOfRepeatsRandom = repeatsCheck.checked;
+//     document.getElementById('num-repeats-slider').style.opacity = 0.9;
+// }
 
-repeatsCheck.oninput = () => {
-    numOfRepeatsRandom = repeatsCheck.checked;
-    if (repeatsCheck.checked === true) {
-        document.getElementById('num-repeats-slider').style.opacity = 0.3;
-        numRepeatsSliderOutput.innerHTML = '...';
-    } else {
-        document.getElementById('num-repeats-slider').style.opacity = 0.9;
-        numRepeatsSliderOutput.innerHTML = numRepeatsSlider.value;
-    }
-}
+// repeatsCheck.oninput = () => {
+//     numOfRepeatsRandom = repeatsCheck.checked;
+//     if (repeatsCheck.checked === true) {
+//         document.getElementById('num-repeats-slider').style.opacity = 0.3;
+//         numRepeatsSliderOutput.innerHTML = '...';
+//     } else {
+//         document.getElementById('num-repeats-slider').style.opacity = 0.9;
+//         numRepeatsSliderOutput.innerHTML = numRepeatsSlider.value;
+//     }
+// }
 
-// number of chords
-var numChordsSlider = document.getElementById("num-chords-slider");
-var numChordsSliderOutput = document.getElementById("num-chords-output");
-var chordsCheck = document.getElementById("numOfChords-check");
+// // number of chords
+// var numChordsSlider = document.getElementById("num-chords-slider");
+// var numChordsSliderOutput = document.getElementById("num-chords-output");
+// var chordsCheck = document.getElementById("numOfChords-check");
 
-numChordsSliderOutput.innerHTML = '...';
-numChordsSlider.oninput = function () {
-    numChordsSliderOutput.innerHTML = this.value;
-    onScreenFirstPassOptions.numOfChords = this.value;
-    chordsCheck.checked = false;
-    numOfChordsRandom = chordsCheck.checked;
-    document.getElementById('num-chords-slider').style.opacity = 0.9;
-}
+// numChordsSliderOutput.innerHTML = '...';
+// numChordsSlider.oninput = function () {
+//     numChordsSliderOutput.innerHTML = this.value;
+//     onScreenFirstPassOptions.numOfChords = this.value;
+//     chordsCheck.checked = false;
+//     numOfChordsRandom = chordsCheck.checked;
+//     document.getElementById('num-chords-slider').style.opacity = 0.9;
+// }
 
-chordsCheck.oninput = () => {
-    numOfChordsRandom = chordsCheck.checked;
-    if (chordsCheck.checked === true) {
-        document.getElementById('num-chords-slider').style.opacity = 0.3;
-        numChordsSliderOutput.innerHTML = '...';
-    } else {
-        document.getElementById('num-chords-slider').style.opacity = 0.9;
-        numChordsSliderOutput.innerHTML = numChordsSlider.value;
-    }
-}
+// chordsCheck.oninput = () => {
+//     numOfChordsRandom = chordsCheck.checked;
+//     if (chordsCheck.checked === true) {
+//         document.getElementById('num-chords-slider').style.opacity = 0.3;
+//         numChordsSliderOutput.innerHTML = '...';
+//     } else {
+//         document.getElementById('num-chords-slider').style.opacity = 0.9;
+//         numChordsSliderOutput.innerHTML = numChordsSlider.value;
+//     }
+// }
 
-// start progression on given harmony
-var startingChordSlider = document.getElementById("starting-chord-slider");
-var startingChordOutput = document.getElementById("starting-chord-output");
-var startingChordCheck = document.getElementById("startingChord-check");
+// // start progression on given harmony
+// var startingChordSlider = document.getElementById("starting-chord-slider");
+// var startingChordOutput = document.getElementById("starting-chord-output");
+// var startingChordCheck = document.getElementById("startingChord-check");
 
-if (startingChordCheck.checked === false ) {
-    startingChordSlider.style.opacity = 0.9;
-}
+// if (startingChordCheck.checked === false ) {
+//     startingChordSlider.style.opacity = 0.9;
+// }
 
-startingChordOutput.innerHTML = currentHarmony[startingChordSlider.value - 1];
-startingChordSlider.oninput = function () {
-    startingChordOutput.innerHTML = currentHarmony[this.value - 1];
-    onScreenFirstPassOptions.startingChord = this.value;
-    startingChordCheck.checked = false;
-    startingChordRandom = startingChordCheck.checked;
-    document.getElementById('starting-chord-slider').style.opacity = 0.9;
-}
+// startingChordOutput.innerHTML = currentHarmony[startingChordSlider.value - 1];
+// startingChordSlider.oninput = function () {
+//     startingChordOutput.innerHTML = currentHarmony[this.value - 1];
+//     onScreenFirstPassOptions.startingChord = this.value;
+//     startingChordCheck.checked = false;
+//     startingChordRandom = startingChordCheck.checked;
+//     document.getElementById('starting-chord-slider').style.opacity = 0.9;
+// }
 
-startingChordCheck.oninput = () => {
-    startingChordRandom = startingChordCheck.checked;
-    if (startingChordCheck.checked === true) {
-        document.getElementById('starting-chord-slider').style.opacity = 0.3;
-        startingChordOutput.innerHTML = '...';
-    } else {
-        document.getElementById('starting-chord-slider').style.opacity = 0.9;
-        startingChordOutput.innerHTML = currentHarmony[startingChordSlider.value - 1];
-    }
-}
-
-
+// startingChordCheck.oninput = () => {
+//     startingChordRandom = startingChordCheck.checked;
+//     if (startingChordCheck.checked === true) {
+//         document.getElementById('starting-chord-slider').style.opacity = 0.3;
+//         startingChordOutput.innerHTML = '...';
+//     } else {
+//         document.getElementById('starting-chord-slider').style.opacity = 0.9;
+//         startingChordOutput.innerHTML = currentHarmony[startingChordSlider.value - 1];
+//     }
+// }
 
 
 
@@ -96,40 +94,42 @@ startingChordCheck.oninput = () => {
 
 
 
-// major / minor
-let modeArray = [major, minor];
-var keyModeSlider = document.getElementById("key-mode-slider");
-var keyModeSliderOutput = document.getElementById("key-mode-output");
-var keyModeCheck = document.getElementById("keyMode-check");
 
-keyModeSliderOutput.innerHTML = "...";
-keyModeSlider.oninput = function () {
-    if (Number(this.value) === 1) {
-        keyModeSliderOutput.innerHTML = "Major";
-    } else {
-        keyModeSliderOutput.innerHTML = "Minor";
-    }
-    keyModeCheck.checked = false;
-    keyModeRandom = startingChordCheck.checked;
-    onScreenFirstPassOptions.keyModeSwitch = modeArray[this.value - 1];
-    document.getElementById('key-mode-slider').style.opacity = 0.9;
-}
 
-keyModeCheck.oninput = () => {
-    keyModeRandom = keyModeCheck.checked;
-    if (keyModeCheck.checked === true) {
-        document.getElementById('key-mode-slider').style.opacity = 0.3;
-        keyModeSliderOutput.innerHTML = '...';
-    } else {
-        document.getElementById('key-mode-slider').style.opacity = 0.9;
-        if (Number(keyModeSlider.value) === 1) {
-            keyModeSliderOutput.innerHTML = "Major";
-        } else {
-            keyModeSliderOutput.innerHTML = "Minor";
-        }
-        onScreenFirstPassOptions.keyModeSwitch = modeArray[keyModeSlider.value - 1];
-    }
-}
+// // major / minor
+// let modeArray = [major, minor];
+// var keyModeSlider = document.getElementById("key-mode-slider");
+// var keyModeSliderOutput = document.getElementById("key-mode-output");
+// var keyModeCheck = document.getElementById("keyMode-check");
+
+// keyModeSliderOutput.innerHTML = "...";
+// keyModeSlider.oninput = function () {
+//     if (Number(this.value) === 1) {
+//         keyModeSliderOutput.innerHTML = "Major";
+//     } else {
+//         keyModeSliderOutput.innerHTML = "Minor";
+//     }
+//     keyModeCheck.checked = false;
+//     keyModeRandom = startingChordCheck.checked;
+//     onScreenFirstPassOptions.keyModeSwitch = modeArray[this.value - 1];
+//     document.getElementById('key-mode-slider').style.opacity = 0.9;
+// }
+
+// keyModeCheck.oninput = () => {
+//     keyModeRandom = keyModeCheck.checked;
+//     if (keyModeCheck.checked === true) {
+//         document.getElementById('key-mode-slider').style.opacity = 0.3;
+//         keyModeSliderOutput.innerHTML = '...';
+//     } else {
+//         document.getElementById('key-mode-slider').style.opacity = 0.9;
+//         if (Number(keyModeSlider.value) === 1) {
+//             keyModeSliderOutput.innerHTML = "Major";
+//         } else {
+//             keyModeSliderOutput.innerHTML = "Minor";
+//         }
+//         onScreenFirstPassOptions.keyModeSwitch = modeArray[keyModeSlider.value - 1];
+//     }
+// }
 
 
 
@@ -200,31 +200,31 @@ keyModeCheck.oninput = () => {
 
 
 
-// type of cadence 
-var cadenceSlider = document.getElementById("cadence-slider");
-var cadenceSliderOutput = document.getElementById("cadence-output");
-var cadenceCheck = document.getElementById("cadence-check");
+// // type of cadence 
+// var cadenceSlider = document.getElementById("cadence-slider");
+// var cadenceSliderOutput = document.getElementById("cadence-output");
+// var cadenceCheck = document.getElementById("cadence-check");
 
-cadenceSliderOutput.innerHTML = '...';
-cadenceSlider.oninput = function () {
-    cadenceSliderOutput.innerHTML = cadenceType[this.value - 1];
-    onScreenFirstPassOptions.typeOfCadence = this.value;
-    cadenceCheck.checked = false;
-    typeOfCadenceRandom = cadenceCheck.checked;
-    document.getElementById('cadence-slider').style.opacity = 0.9;
-}
+// cadenceSliderOutput.innerHTML = '...';
+// cadenceSlider.oninput = function () {
+//     cadenceSliderOutput.innerHTML = cadenceType[this.value - 1];
+//     onScreenFirstPassOptions.typeOfCadence = this.value;
+//     cadenceCheck.checked = false;
+//     typeOfCadenceRandom = cadenceCheck.checked;
+//     document.getElementById('cadence-slider').style.opacity = 0.9;
+// }
 
-cadenceCheck.oninput = () => {
-    typeOfCadenceRandom = cadenceCheck.checked;
-    if (cadenceCheck.checked === true) {
-        document.getElementById('cadence-slider').style.opacity = 0.3;
-        cadenceSliderOutput.innerHTML = '...';
-    } else {
-        document.getElementById('cadence-slider').style.opacity = 0.9;
-        cadenceSliderOutput.innerHTML = cadenceType[Number(cadenceSlider.value) - 1];
-        onScreenFirstPassOptions.typeOfCadence = Number(cadenceSlider.value);
-    }
-}
+// cadenceCheck.oninput = () => {
+//     typeOfCadenceRandom = cadenceCheck.checked;
+//     if (cadenceCheck.checked === true) {
+//         document.getElementById('cadence-slider').style.opacity = 0.3;
+//         cadenceSliderOutput.innerHTML = '...';
+//     } else {
+//         document.getElementById('cadence-slider').style.opacity = 0.9;
+//         cadenceSliderOutput.innerHTML = cadenceType[Number(cadenceSlider.value) - 1];
+//         onScreenFirstPassOptions.typeOfCadence = Number(cadenceSlider.value);
+//     }
+// }
 
 // key center
 let keyCenterArray = [keyOfF, keyOfC, keyOfG, keyOfD];
@@ -268,6 +268,17 @@ keyCenterCheck.oninput = () => {
         onScreenFirstPassOptions.keyCenter = keyCenterArray[Number(keyCenterSlider.value) - 1];
     }
 }
+
+
+
+
+
+
+
+
+
+
+
 
 // On-screen diagnostic/helper functions 
 
